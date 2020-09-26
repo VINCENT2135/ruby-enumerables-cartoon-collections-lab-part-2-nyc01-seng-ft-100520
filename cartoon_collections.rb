45d5c3b2d6a8de8f8d5f29a8238312b700fc7331
Vincent Ivos <vincentivos1@gmail.com>
2020-09-26 18:44:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -35,7 +35,7 @@ def long_planeteer_calls(planeteer_calls)
 def find_valid_calls(planeteer_calls)
  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |planeteer_calls|
-    cheese_types.include?(cheese)
+    valid_calls.include?(planeteer_calls)
   end
 end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
