d96e7e0d17500eefa7aa2fa7039d9436ba3780f0
Vincent Ivos <vincentivos1@gmail.com>
2020-09-26 18:42:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,6 +33,7 @@ def long_planeteer_calls(planeteer_calls)
 
 
 def find_valid_calls(planeteer_calls)
+ valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
  planeteer_calls.find do |element|
   planeteer_calls.include?(element)
   end
